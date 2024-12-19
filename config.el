97bcd2f38c94252f764f90deecc695ee0b8addfc
HW home <freizl@gmail.com>
2024-12-18 17:06:26 -0800

    sync

--- config.el
+++ config.el
@@ -665,13 +665,13 @@
 ;; (setq doom-variable-pitch-font (font-spec :family "Linux Libertine O" :size 15))
 ;; (setq doom-variable-pitch-font (font-spec :family "ETBembo" :size 17))
 ;;
-;; (setq doom-font (font-spec :family "Iosevka SS08" :size 15 :weight 'regular)
-;;       doom-serif-font (font-spec :family "Iosevka Etoile" :size 15 :weight 'regular)
-;;       doom-variable-pitch-font (font-spec :family "Comic Sans MS" :size 15 :weight 'regular))
+(setq doom-font (font-spec :family "Iosevka SS08" :size 15 :weight 'regular)
+      doom-serif-font (font-spec :family "Iosevka Etoile" :size 15 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Iosevka SS08" :size 15 :weight 'regular))
 ;;
-(setq doom-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular)
-      doom-serif-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular))
+;; (setq doom-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular)
+;;       doom-serif-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular)
+;;       doom-variable-pitch-font (font-spec :family "Berkeley Mono" :size 15 :weight 'regular))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
